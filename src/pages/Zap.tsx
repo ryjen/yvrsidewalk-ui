8ecf58b994563c9c7d26b87ca679da01c9f1e5b8
Ryan Jennings <rybertjen@gmail.com>
2024-01-03 20:30:17 -0800

    fix: update zap ui

--- src/pages/Zap.tsx
+++ src/pages/Zap.tsx
@@ -1,14 +1,43 @@
 import ResponsiveAppBar from "../components/ResponsiveAppBar";
+import { useState } from "react";
 
-const Zap = () => (
-  <>
-    <ResponsiveAppBar />
+const Zap = () => {
+  const [state, setState] = useState("");
 
-    <form action="https://localhost:4000/zap">
-      <input name="amount" />
-      <button type="submit">Zap</button>
-    </form>
-  </>
-);
+  const zapper = async (e: React.SyntheticEvent) => {
+    e.preventDefault();
+    const target = e.target as typeof e.target & {
+      amount: { value: number };
+      memo: { value: string };
+    };
+    console.log(target);
+    const { amount, memo } = target;
+    try {
+      const response = await fetch("https://localhost:4000/zap", {
+        method: "POST",
+        body: JSON.stringify({ amount, memo }),
+      });
+      const { payment_hash } = await response.json();
+      setState(payment_hash);
+    } catch (error) {
+      console.error("Error creating LNBits invoice:", error);
+    }
+  };
+  return (
+    <>
+      <ResponsiveAppBar />
+
+      <form onSubmit={zapper}>
+        <label htmlFor="amount">Amount:</label>
+        <input id="amount" name="amount" type="number" required />
+        <label htmlFor="memo">Memo:</label>
+        <input id="memo" name="memo" type="text" required />
+        <button type="submit">Zap</button>
+      </form>
+
+      <div>{state}</div>
+    </>
+  );
+};
 
 export default Zap;
